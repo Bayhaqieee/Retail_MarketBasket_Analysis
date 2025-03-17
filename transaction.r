d7b79cb2b24c3e92477f9a4c00a74e0befb2d165
Muhammad Aditya Bayhaqie <adityabayhaqie@gmail.com>
2025-03-17 12:41:20 +0700

    Transaction Function

--- transaction.r
+++ transaction.r
@@ -1,4 +1,6 @@
+# Block Spesific Lines on this code for more select-based code instead of running all code
 library(arules)
+
 # transaksi <- read.transactions(file="https://storage.googleapis.com/dqlab-dataset/data_transaksi.txt", format="single", sep="\t", cols=c(1,2), skip=1)
 transaksi <- read.transactions(file="Retail_MarketBasketAnalysis/data_transaksi3.txt", format="single", sep="\t", cols=c(1,2), skip=1)
 transaksi@itemInfo
@@ -9,3 +11,24 @@ transaksi@data
 itemFrequency(transaksi)
 itemFrequency(transaksi, type="absolute") # absolute frequency
 itemFrequency(transaksi, type="relative") # relative frequency
+
+#Tampilan item frequency plot
+itemFrequencyPlot(transaksi)
+
+#Menggunakan inspect terhadap transaksi
+inspect(transaksi)
+
+#Menghasilkan associaton rules
+apriori(transaksi)
+
+#Menghasilkan association rules dan disimpan sebagai variable mba
+mba <- apriori(transaksi)
+
+#Melihat isi dari rules dengan menggunakan fungsi inspect
+inspect(mba)
+
+#Menghasilkan association rules dan disimpan sebagai variable mba
+aba <- apriori(transaksi)
+
+#Filter rhs dengan item "Sirup" dan tampilkan 
+inspect(subset(aba, rhs %in% "Sirup"))
